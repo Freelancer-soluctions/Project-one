ccece8b257afbbbc7aed4f588f196d7997539849
lpfalcon <lpfalcon99@gmail.com>
2024-05-27 22:58:25 -0500

    add tanstack note datatable

--- client/src/modules/note/pages/Note.jsx
+++ client/src/modules/note/pages/Note.jsx
@@ -1,74 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import DataTable from "../../../components/dataTable/dataTable";
 import NoteDialog from "../components/dialog";
 import gridColumns from "../components/gridColumns";
+import useGetNotes from "../hooks/useGetNotes";
 
 const Note = () => {
-  const data =  [
-    {
-      "id": 1,
-      "closedOn": "2024-09-09T00:00:00.000Z",
-      "createdOn": "2020-04-04T00:00:00.000Z",
-      "document": "https://res.cloudinary.com/dc4kwuipu/image/upload/v1716412649/poofrl6rvjcc7cjiksqr.png",
-      "documentId": "poofrl6rvjcc7cjiksqr",
-      "note": "Note description",
-      "statusId": 1,
-      "closedBy": 1,
-      "createdBy": 1,
-      "status": {
-        "id": 1,
-        "code": "CO1",
-        "description": "Active"
-      },
-      "userNoteClosed": {
-        "id": 1,
-        "accessConfiguration": false,
-        "accessNews": false,
-        "address": "address",
-        "birthday": "1995-04-22T16:15:38.959Z",
-        "city": "city",
-        "document": "photo",
-        "documentId": "123",
-        "email": "example@gmail.com",
-        "isAdmin": false,
-        "isManager": false,
-        "lastUpdatedBy": "2024-04-22T16:13:56.128Z",
-        "lastUpdatedOn": "2024-04-22T16:13:37.737Z",
-        "name": "test",
-        "roleId": 1,
-        "socialSecurity": "123",
-        "startDate": "2024-04-22T16:13:31.440Z",
-        "state": "state",
-        "statusId": 1,
-        "telephone": "123",
-        "zipcode": "123"
-      },
-      "userNoteCreated": {
-        "id": 1,
-        "accessConfiguration": false,
-        "accessNews": false,
-        "address": "address",
-        "birthday": "1995-04-22T16:15:38.959Z",
-        "city": "city",
-        "document": "photo",
-        "documentId": "123",
-        "email": "example@gmail.com",
-        "isAdmin": false,
-        "isManager": false,
-        "lastUpdatedBy": "2024-04-22T16:13:56.128Z",
-        "lastUpdatedOn": "2024-04-22T16:13:37.737Z",
-        "name": "test",
-        "roleId": 1,
-        "socialSecurity": "123",
-        "startDate": "2024-04-22T16:13:31.440Z",
-        "state": "state",
-        "statusId": 1,
-        "telephone": "123",
-        "zipcode": "123"
-      }
-    }
-  ]
+
+  const { data: notes, isFetching } = useGetNotes();
+  const onDelete = useCallback(
+    () => console.log('On delete, ;ho'),
+    []
+  )
+
+  const onEdit = useCallback(
+    () => console.log('On edit'),
+    []
+  )
+
   const [showModal, setShowModal] = useState(false)
   return (
     <section className="py-16">
@@ -80,8 +29,8 @@ const Note = () => {
           setShowModal={setShowModal}
           title="Create Note"
         />
+        {!isFetching && <DataTable columns={gridColumns({ onDelete, onEdit })} data={notes.data} />}
 
-        <DataTable columns={gridColumns}  data={data}/>
       </div>
 
     </section>
