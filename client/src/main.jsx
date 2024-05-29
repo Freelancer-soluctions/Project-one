1c9fdb793ee3ea85b891ccde9951a702485bd750
lpfalcon <lpfalcon99@gmail.com>
2024-05-29 17:35:12 -0500

    Add tanStack to note module

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Toaster } from "@/components/ui/toaster"
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// import { TanStackProvider } from '@/plugins/TanStackProvider.tsx';
 
 import App from './App'
-const queryClient = new QueryClient();
+import { TanStackProvider } from './plugins/TanStackProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-
+    <TanStackProvider>
       <App />
-    </QueryClientProvider>
-
+      <Toaster />
+    </TanStackProvider>
   </React.StrictMode>,
 )
--- client/src/modules/note/pages/Note.jsx
+++ client/src/modules/note/pages/Note.jsx
@@ -1,32 +1,44 @@
 import { Button } from "@/components/ui/button";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import DataTable from "../../../components/dataTable/dataTable";
 import NoteDialog from "../components/dialog";
 import gridColumns from "../components/gridColumns";
 import useGetNotes from "../hooks/useGetNotes";
+import useDeleteNoteMutation from "../hooks/useDeleteNoteMutation";
 
 const Note = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [selectedNote, setSelectedNote] = useState(null);
+  const useDeleteMutation = useDeleteNoteMutation()
 
   const { data: notes, isFetching } = useGetNotes();
-  const onDelete = useCallback(
-    () => console.log('On delete, ;ho'),
-    []
-  )
 
-  const onEdit = useCallback(
-    () => console.log('On edit'),
-    []
-  )
+  const onDelete = (selectedRow) => useDeleteMutation.mutate(selectedRow.id)
 
-  const [showModal, setShowModal] = useState(false)
+
+  const onEdit = (selectedRow) => {
+    setSelectedNote(selectedRow)
+    setIsDialogOpen(true)
+  }
+
+
+
+  const onOpenChangeDialog = (value) => {
+    setIsDialogOpen(value)
+    if (!value) {
+      setSelectedNote(null)
+    }
+  }
+
   return (
     <section className="py-16">
       <div className="container">
         <h1 className="text-3xl font-bold">Notes</h1>
-        <Button onClick={() => setShowModal(true)}> Create Note </Button>
+        <Button onClick={() => setIsDialogOpen(true)}> Create Note </Button>
         <NoteDialog
-          showModal={showModal}
-          setShowModal={setShowModal}
+          isDialogOpen={isDialogOpen}
+          onOpenChange={onOpenChangeDialog}
+          note={selectedNote}
           title="Create Note"
         />
         {!isFetching && <DataTable columns={gridColumns({ onDelete, onEdit })} data={notes.data} />}
