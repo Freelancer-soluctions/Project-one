f6427f5e1f1550dede9be2feccee8f3af31106f6
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-06-21 23:53:59 -0500

    Rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,41 @@
+import { lazy } from "react";
 import Layout from "./components/layout";
-import Login from "./modules/auth/pages/Login";
+// import Home from "./modules/home/pages/Home";
+
 import NotFound from "./components/404/NotFound";
 import routes from "./routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const App = () => {
-
-    const router = createBrowserRouter([
-
-            // element: <LandingPage />, para mas adelante
-        // {
-        //     element: <Login />,
-        //     children: routes
-        // }
-        
-          {
-            
-            element: <Login />,
-            children:[
-                {
-                    
-
-                    element: <Layout />,
-                    children: routes
-                }
-              
-            ]
-           
+  const router = createBrowserRouter([
+ 
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/signIn",
+          Component: lazy(() => import("@/modules/auth/pages/SignIn")),
         },
         {
-            path: "/*",
-            element: <NotFound />,
+          path: "/signUp",
+          Component: lazy(() => import("@/modules/auth/pages/SignUp")),
         },
+        {
+          path: "/home",
+          element: lazy(() => import("@/modules/home/pages/Home")),
+          children: routes,
+        },
+      ],
 
-    ])
+      // errorElement:  <NotFound />, // vere diferencia con /*
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+  ]);
 
-    return <RouterProvider router={ router } />;
-
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,17 +1,15 @@
 import { lazy } from "react";
 import PathConstants from "./pathConstants";
 
-const Home = lazy(()=> import('@/modules/home/pages/Home'));
-const Auth = lazy(()=> import('@/modules/auth/pages/Login'));
-const Note = lazy(()=> import('@/modules/note/pages/Note'));
-
+// const Home = lazy(()=> import('@/modules/home/pages/Home'));
+// const Auth = lazy(()=> import('@/modules/auth/pages/Login'));
+const Note = lazy(() => import("@/modules/note/pages/Note"));
 
 const routes = [
-    { index: true, element: <Home/> },
-    { path: PathConstants.AUTH, element: <Auth /> },
-    { path: PathConstants.NOTE, element: <Note/> }
-//     lazy(() => import("./Admin/Users/EditUser")
-    ]
+  // { index: true, element: <Home/> },
+  // { path: PathConstants.AUTH, element: <Auth /> },
+  { path: PathConstants.NOTE, element: <Note /> },
+  //     lazy(() => import("./Admin/Users/EditUser")
+];
 
-
-export default routes
+export default routes;
