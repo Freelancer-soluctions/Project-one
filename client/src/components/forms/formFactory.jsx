78663aa424e06076c7c6a1b8eb29c148cbbc6ef9
lpfalcon <lpfalcon99@gmail.com>
2024-06-26 17:31:24 -0500

    fix formFactory.jsx

--- client/src/components/forms/formFactory.jsx
+++ client/src/components/forms/formFactory.jsx
@@ -27,112 +27,95 @@ import PropTypes from "prop-types";
 
 const FormFactory = ({ fieldProps, control }) => {
 
-    const renderFieldComponent = {
-        textArea: (field) => (
-            < >
-                <FormControl>
-                    <Textarea
-                        placeholder={fieldProps.placeholder}
-                        className="resize-none"
-                        {...field}
-                    />
-                </FormControl>
-            </>
+    const {
+        componentType, label, name,
+        itemClassName, placeholder, accept,
+        descriptionField, items
+    } = fieldProps;
 
+    const renderField = {
+        textArea: (field) => (
+            <FormControl>
+                <Textarea
+                    placeholder={placeholder}
+                    className="resize-none"
+                    {...field}
+                />
+            </FormControl>
         ),
         inputFile: (field) => (
-            <>
-                <FormControl>
-                    <Input
-                        type="file"
-                        accept={fieldProps.accept}
-                        onChange={(event) => {
-                            field.onChange(event.target.files ? event.target.files[0] : null)
-
-                        }}
-                    />
-                </FormControl>
-            </>
+            <FormControl>
+                <Input
+                    type="file"
+                    accept={accept}
+                    onChange={(event) => field.onChange(event.target.files?.[0])}
+                />
+            </FormControl>
         ),
         selectIdValue: (field) => (
-            <>
-                <Select
-                    value={field.value.toString()}
-                    onValueChange={(value) => field.onChange(Number(value))}
-                    defaultValue={field.value.toString()}
-                >
-                    <FormControl>
-                        <SelectTrigger>
-                            <SelectValue placeholder={fieldProps.placeholder} />
-                        </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                        {
-                            fieldProps.items.map((item) => (
-                                <SelectItem key={item.id}
-                                    value={item.id.toString()}
-                                >
-                                    {item[fieldProps.descriptionField]}
-                                </SelectItem>
-                            ))
-                        }
-                    </SelectContent>
-                </Select>
-            </>
+            <Select
+                value={field.value?.toString()}
+                onValueChange={(value) => field.onChange(Number(value))}
+                defaultValue={field.value?.toString()}
+            >
+                <FormControl>
+                    <SelectTrigger>
+                        <SelectValue placeholder={placeholder} />
+                    </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                    {
+                        items.map((item) => (
+                            <SelectItem key={item.id}
+                                value={item.id.toString()}
+                            >
+                                {item[descriptionField]}
+                            </SelectItem>
+                        ))
+                    }
+                </SelectContent>
+            </Select>
         ),
         date: (field) => (
-            <>
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <FormControl>
-                            <Button
-                                variant={"outline"}
-                                className={cn(
-                                    "pl-3 text-left font-normal",
-                                    !field.value && "text-muted-foreground"
-                                )}
-                            >
-                                {field.value ? (
-                                    format(field.value, "yyyy-MM-dd")
-                                ) : (
-                                    <span>{fieldProps.placeholder}</span>
-                                )}
-
-                            </Button>
-                        </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                            mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            disabled={(date) =>
-                                date > new Date() || date < new Date("1900-01-01")
-                            }
-                            initialFocus
-                        />
-                    </PopoverContent>
-                </Popover>
-            </>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <FormControl>
+                        <Button
+                            variant="outline"
+                            className={cn(
+                                "pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                            )}
+                        >
+                            {field.value ?
+                                format(field.value, "yyyy-MM-dd")
+                                : <span>{placeholder}</span>}
+                        </Button>
+                    </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                        initialFocus
+                    />
+                </PopoverContent>
+            </Popover>
         )
-    }
+    };
 
     return (
-        <>
-            <FormField
-                control={control}
-                name={fieldProps.name}
-                render={({ field }) => (
-                    <FormItem className={ fieldProps.itemClassName}>
-                        <FormLabel>{fieldProps.label}</FormLabel>
-                        {renderFieldComponent[fieldProps.componentType](field)}
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-        </>
-    )
-}
+        <FormField control={control} name={name} render={({ field }) => (
+            <FormItem className={itemClassName}>
+                <FormLabel>{label}</FormLabel>
+                {renderField[componentType](field)}
+                <FormMessage />
+            </FormItem>
+        )} />
+    );
+};
 
 FormFactory.propTypes = {
 
