8265bba951a4cc6a30938a3e372d233c19330b4d
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-06-28 23:45:08 -0500

    fixes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -5,11 +5,10 @@ const Access = lazy(()=> import('@/modules/access/pages/Access'));
 const Note = lazy(() => import("@/modules/note/pages/Note"));
 
 const routes = [
-  // { index: true, element: <Home/> },
-  // { path: PathConstants.AUTH, element: <Auth /> },
+
   { index: true, element: <Access/>},
   { path: PathConstants.NOTE, element: <Note /> },
-  //     lazy(() => import("./Admin/Users/EditUser")
+
 ];
 
 export default routes;
