3a95c615604b2a7bda5143b59c897772fe2e8912
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-07-26 18:49:49 -0500

    new changes

--- client/src/modules/news/pages/News.jsx
+++ client/src/modules/news/pages/News.jsx
@@ -1,4 +1,10 @@
 const News = () => {
-  return <div> Bienvenido a news</div>
+  return (
+    <div className='justify-center flex-1 p-4 overflow-auto md:p-6 border-1'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1'>
+        hola munfo feliz
+      </div>
+    </div>
+  )
 }
 export default News
