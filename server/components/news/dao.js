60136374f4f226c8d4cc5699bccd88d360a0d633
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-08-04 23:44:17 -0500

    New changes in orm

--- server/components/news/dao.js
+++ server/components/news/dao.js
@@ -11,24 +11,74 @@ const tableName = 'news'
  * @param {Date} fromDate
  * @returns all news from db
  */
-export const getAllNews = async (description, statusCode, toDate, fromDate) => {
-  const news = await prisma.news.findMany({
+export const getAllNews = async (description, fromDate, toDate, statusCode) => {
+  console.log(description, fromDate, toDate, statusCode)
+  const news = await prisma.newsStatus.findMany({
     where: {
-      description: { contains: description },
-      createdOn: {
-        gte: new Date(fromDate),
-        lte: new Date(toDate)
+      ...(statusCode
+        ? {
+            AND: [
+              {
+                code: { equals: statusCode }
+              }
+            ]
+          }
+        : {})
 
+    },
+    include: {
+      news: {
+        where: {
+          ...(description
+            ? {
+                AND: [
+                  {
+                    description: { contains: description }
+                  }
+                  // {
+                  //   NOT: { description: null }
+                  // }
+                ]
+              }
+            : {}),
+
+          ...((fromDate && toDate)
+            ? {
+                AND: [
+                  {
+                    createdOn: {
+                      gte: new Date(fromDate),
+                      lte: new Date(toDate)
+
+                    }
+                  }
+                ]
+              }
+            : {})
+        }
       }
+    }
+
+  })
+
+  const data = news.map((item) => {
+    return {
+      ...item.news,
+      status: item.description
 
     }
   })
 
-  return Promise.resolve(news)
+  return Promise.resolve(data)
 }
 
 export const getAllNewsStatus = async () => {
-  const news = await prisma.newsStatus.findMany()
+  const statusId = 1
+  const news = await prisma.newsStatus.findMany({
+    include: {
+      news: { where: { statusId } }
+    }
+  })
   return Promise.resolve(news)
 }
 
