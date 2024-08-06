cb604d84eedc0bab15c7121ecd297417c87061d8
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-08-05 22:52:50 -0500

    Tanstack table

--- server/components/news/dao.js
+++ server/components/news/dao.js
@@ -12,72 +12,80 @@ const tableName = 'news'
  * @returns all news from db
  */
 export const getAllNews = async (description, fromDate, toDate, statusCode) => {
-  console.log(description, fromDate, toDate, statusCode)
-  const news = await prisma.newsStatus.findMany({
-    where: {
-      ...(statusCode
-        ? {
-            AND: [
-              {
-                code: { equals: statusCode }
-              }
-            ]
-          }
-        : {})
+  const news = await prisma.news.findMany(
+    {
 
-    },
-    include: {
-      news: {
-        where: {
-          ...(description
-            ? {
-                AND: [
-                  {
-                    description: { contains: description }
+      where: {
+        ...(description
+          ? {
+              AND: [
+                {
+                  description: { contains: description }
+                }
+              // {
+              //   NOT: { description: null }
+              // }
+              ]
+            }
+          : {}),
+
+        ...((fromDate && toDate)
+          ? {
+              AND: [
+                {
+                  createdOn: {
+                    gte: new Date(fromDate),
+                    lte: new Date(toDate)
+
                   }
-                  // {
-                  //   NOT: { description: null }
-                  // }
-                ]
-              }
-            : {}),
+                }
+              ]
+            }
+          : {}),
+        ...(statusCode
+          ? {
+              status:
+                {
+                  code: { equals: statusCode }
+                }
 
-          ...((fromDate && toDate)
-            ? {
-                AND: [
-                  {
-                    createdOn: {
-                      gte: new Date(fromDate),
-                      lte: new Date(toDate)
+            }
+          : {})
+        // status: { code: { equals: statusCode } }
 
-                    }
-                  }
-                ]
-              }
-            : {})
-        }
+      },
+      include: {
+        status: { select: { description: true } },
+        userNewsCreated: { select: { name: true } },
+        userNewsClosed: { select: { name: true } }
+
       }
-    }
 
-  })
+      // select: {
+      //   id: true,
+      //   closedOn: true,
+      //   createdOn: true,
+      //   description: true,
+      //   document: true,
+      //   documentId: true,
+      //   statusId: true,
+      //   closedBy: true,
+      //   createdBy: true,
+      //   status: { select: { description: true } }
 
-  const data = news.map((item) => {
-    return {
-      ...item.news,
-      status: item.description
+      // }
 
-    }
-  })
+    })
 
-  return Promise.resolve(data)
+  return Promise.resolve(news)
 }
 
 export const getAllNewsStatus = async () => {
-  const statusId = 1
+  // const statusId = 1
   const news = await prisma.newsStatus.findMany({
-    include: {
-      news: { where: { statusId } }
-    }
+    // include: {
+    //   news: { where: { statusId } }
+    // }
   })
   return Promise.resolve(news)
 }
