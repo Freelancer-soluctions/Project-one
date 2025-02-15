1343ddf8dd2eb95598751ca2c0f867d06e7f3b05
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-02-15 01:44:32 -0500

    feat: storybook configuration

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ Front end
 - Persitencia de datos como el inicio de sesion con redux-persist
 - Proteccion de rutas solo accesibles cuando la sesion es valida por medio de un guard
 - Internalizacion con i18next y react-i18next soportando dos idiomas (ingles y español)
-- 
+- Storybook para la documentacion y prueba componentes UI, para la facilitacion entre diseñadores y QA testers
+
+Back end
+- Swagger ui para la documentacion y prueba APIs REST
--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,12 +1,16 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  // stories: [
+  //   "../stories/**/*.stories.@(js|jsx|ts|tsx)", // Asegura que esta ruta incluya la carpeta "ui"
+  // ],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    "@storybook/addon-postcss"
   ],
   framework: {
     name: '@storybook/react-vite',
