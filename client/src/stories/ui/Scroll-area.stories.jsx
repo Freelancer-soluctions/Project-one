757d0dde52d05aeef52953098c3b3b153e010333
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-02-15 23:55:03 -0500

    feat: shadn stories

---
+++ client/src/stories/ui/Scroll-area.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+
+export default {
+  title: 'UI/ScrollArea',
+  component: ScrollArea,
+  parameters: {
+    layout: 'centered'
+  }
+}
+
+export const ScrollAreaStory = () => (
+  <ScrollArea className='p-4 border h-60 w-80'>
+    <div className='h-[200%] w-full'>Contenido largo para hacer scroll...</div>
+    <ScrollBar />
+  </ScrollArea>
+)
