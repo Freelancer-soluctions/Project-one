decfa74c3496af4bd09fba88f53c40ee4622341d
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-02-24 22:46:41 -0500

    feat: display settings

---
+++ client/src/modules/settings/hooks/useSaveDisplaySettings.js
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { saveSettingDisplayFetch } from '../slice/settingsSlice'
+
+export const useSaveDisplaySettings = settings => {
+  const dispatch = useDispatch()
+
+  const onSaveDisplaySettings = async values => {
+    try {
+      const displaySettings = settings.id
+        ? { id: settings.id, displayOptions: { ...values } }
+        : { displayOptions: { ...values } }
+
+      dispatch(saveSettingDisplayFetch(displaySettings))
+      // 🔹 Refrescar la configuración desde el backend para asegurar datos actualizados
+      //await dispatch(getSettingsByUserIdFetch())
+    } catch (error) {
+      console.error('Error updating display settings:', error)
+    }
+  }
+
+  return { onSaveDisplaySettings }
+}
--- client/src/modules/settings/slice/settingsSlice.js
+++ client/src/modules/settings/slice/settingsSlice.js
@@ -2,8 +2,7 @@
 //    hooks corresponding to the defined endpoints */
 //    import {createApi } from '@reduxjs/toolkit/query/react'
 //    import {axiosPrivateBaseQuery} from '@/config/axios'
-   
-   
+
 //    // Define a service using a base URL and expected endpoints
 //    const settingsApi = createApi({
 //        reducerPath:'settingsApi',
@@ -29,60 +28,74 @@
 //                  body,
 //                }
 //              },
-        
+
 //            }),
-     
-          
+
 //          }),
-       
+
 //    })
-   
+
 //    // Export hooks for usage in functional components, which are
 //    // auto-generated based on the defined endpoints
 //    export const { useGetSettingLanguageByIdQuery , useSaveSettingLanguageMutation} = settingsApi
-   
+
 //    export default settingsApi
-   
 
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import {GetSettingsByUserIdFetch, SaveLanguage} from '@/modules/settings/api/settingsAPI'
+import {
+  GetSettingsByUserIdFetch,
+  SaveLanguage,
+  SaveDisplaySettings
+} from '@/modules/settings/api/settingsAPI'
 
-export const getSettingsByUserIdFetch = createAsyncThunk('settings/getSettingsByUserIdFetch', async (args, { rejectWithValue }) => {
-  try {
-    const response = await GetSettingsByUserIdFetch(args)
-    return response.data
-  } catch (error) {
-    return rejectWithValue(error.response.data)
+export const getSettingsByUserIdFetch = createAsyncThunk(
+  'settings/getSettingsByUserIdFetch',
+  async (args, { rejectWithValue }) => {
+    try {
+      const response = await GetSettingsByUserIdFetch(args)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
   }
-})
+)
 
-
-export const saveSettingLanguageFetch = createAsyncThunk('settings/saveLanguage', async (args, { rejectWithValue }) => {
-  try {
-    const response = await SaveLanguage(args)
-    return response.data
-  } catch (error) {
-    return rejectWithValue(error.response.data)
+export const saveSettingLanguageFetch = createAsyncThunk(
+  'settings/saveLanguage',
+  async (args, { rejectWithValue }) => {
+    try {
+      const response = await SaveLanguage(args)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
   }
-})
-
-
-
+)
+export const saveSettingDisplayFetch = createAsyncThunk(
+  'settings/display',
+  async (args, { rejectWithValue }) => {
+    try {
+      const response = await SaveDisplaySettings(args)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
 
 const settingsSlice = createSlice({
   name: 'settings',
   initialState: {
     isLoading: false,
-    dataSettings: {userSettings:null},
+    dataSettings: { userSettings: null },
     isError: false,
-    errorMessage:''
+    errorMessage: ''
   },
   // reducers: {
- 
- 
+
   // },
-  extraReducers: (builder) => {
-    // sign up
+  extraReducers: builder => {
+    // getSettingsByUserId
     builder.addCase(getSettingsByUserIdFetch.pending, (state, action) => {
       state.isLoading = true
       state.isError = false
@@ -90,16 +103,58 @@ const settingsSlice = createSlice({
     builder.addCase(getSettingsByUserIdFetch.fulfilled, (state, action) => {
       state.isLoading = false
       state.isError = false
-      state.dataSettings.userSettings = action.payload 
+      state.dataSettings.userSettings = action.payload
     })
     builder.addCase(getSettingsByUserIdFetch.rejected, (state, action) => {
       console.log('Error', action.error.message)
-      // console.log('Error payload', action.payload.error)
       state.isError = true
       state.isLoading = false
       state.errorMessage = action.error.message
     })
 
+    // saveSettingLanguageFetch
+    builder.addCase(saveSettingLanguageFetch.pending, state => {
+      state.isLoading = true
+      state.isError = false
+    })
+    builder.addCase(saveSettingLanguageFetch.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.isError = false
+
+      // // ✅ Actualizar la configuración del usuario correctamente
+      // if (state.dataSettings.userSettings) {
+      //   state.dataSettings.userSettings.language = action.payload.language
+      // }
+    })
+    builder.addCase(saveSettingLanguageFetch.rejected, (state, action) => {
+      console.log('Error', action.error.message)
+      state.isError = true
+      state.isLoading = false
+      state.errorMessage = action.error.message
+    })
+
+    // saveSettingDisplayFetch
+    builder.addCase(saveSettingDisplayFetch.pending, state => {
+      state.isLoading = true
+      state.isError = false
+    })
+    builder.addCase(saveSettingDisplayFetch.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.isError = false
+
+      // ✅ Actualizar la configuración del usuario correctamente,
+      // Los cambios se reflejan de inmediato en Redux.
+      // No haces una petición extra al backend, lo que mejora la eficiencia.
+      if (state.dataSettings.userSettings) {
+        state.dataSettings.userSettings = action.payload
+      }
+    })
+    builder.addCase(saveSettingDisplayFetch.rejected, (state, action) => {
+      console.log('Error', action.error.message)
+      state.isError = true
+      state.isLoading = false
+      state.errorMessage = action.error.message
+    })
   }
 })
 
