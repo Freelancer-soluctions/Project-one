4706b59c652aa55faf4964a330636f49aa2f6bf9
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-12-04 23:03:25 -0500

    feat: finishing with cryptographic failures

--- server/src/modules/auth/routes.js
+++ server/src/modules/auth/routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as authController from './controller.js'
-import { verifyToken, validateSchema } from '../../middleware/index.js'
+import { verifyToken, validateSchema, loginLimiter, refreshTokenLimiter } from '../../middleware/index.js'
 import { SignUpSchema, SignInSchema } from '../../utils/joiSchemas/joi.js'
 import { verifyCsrf } from '../../middleware/verifyCsrf.js'
 
@@ -8,11 +8,24 @@ const router = Router()
 
 router.post('/signup', validateSchema(SignUpSchema), authController.signUp)
 
-router.post('/signin', validateSchema(SignInSchema), authController.signIn)
+router.post('/signin', loginLimiter, validateSchema(SignInSchema), authController.signIn)
 
 router.get('/session', verifyToken, authController.session)
 
-router.post('/refresh-token', verifyCsrf, authController.refreshToken)
+router.post('/refresh-token', refreshTokenLimiter, verifyCsrf, authController.refreshToken)
+
+// router.post('/change-password',
+//   verifyToken,
+//   changePasswordLimiter,
+//   validateSchema(ChangePasswordSchema),
+//   authController.changePassword
+// )
+
+// router.post('/forgot-password',
+//   forgotPasswordLimiter,
+//   validateSchema(ForgotPasswordSchema),
+//   authController.forgotPassword
+// )
 
 export default router
 
